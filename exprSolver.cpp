c5c7de6eba4212cece0cb0c89f07303914c20acb
shobhit1813 <shobhit.s1813@gmail.com>
2018-08-28 00:34:40 +0530

    initial commit

--- exprSolver.cpp
+++ exprSolver.cpp
@@ -6,11 +6,12 @@
 
 using namespace std;
 
+opStack* oper = NULL;
+valStack* val = NULL;
+char uni = 's';
+
 int solve(char* expr)
 {
-
-	opStack* oper = NULL;
-	valStack* val = NULL;
 	int i;
 	int len = exprLen(expr);
 	int insert_res = 0;
@@ -19,31 +20,38 @@ int solve(char* expr)
 		if(expr[i] == ' ') continue;
 		else if(isdigit(expr[i]))	
 		{
-			int value = 0;
-			while(i < len && isdigit(expr[i]))
-			{
-				value = value * 10 + (expr[i] - '0');
-				i++;
-			}
-			valpush(&val, value);
+			pushDigit(expr, len, i);
 		}
 		else if(expr[i] == '+' || expr[i] == '-' || expr[i] == '*' || expr[i] == '/' || expr[i] == '^' || expr[i] == '%' )
 		{
-			while( !opisEmpty(oper) &&  preceed(optop(oper)) >= preceed(expr[i]) )
+			int j = i;
+			if(expr[i + 1] == ' ')
 			{
-				int val1 = valtop(val);
-				valpop(&val);
-
-				int val2 = valtop(val);
-				valpop(&val);
-
-
-				char op = optop(oper);
-				oppop(&oper);
-				valpush(&val, calculate(val1, val2, op));
+				for(j = i + 1; expr[j] != ' '; j++);
 			}
-			oppush(&oper, expr[i]);
-		}	
+			if(expr[i] == expr[j] || (expr[i] == '+' && expr[j] == '-') || (expr[i] == '-' && expr[j] == '+') )
+			{
+				i = j;
+				sepUniandbin(expr[i], expr[j]);
+			}
+			else
+			{
+					while( !opisEmpty(oper) &&  preceed(optop(oper)) >= preceed(expr[i]) )
+					{
+						int val1 = valtop(val);
+						valpop(&val);
+
+						int val2 = valtop(val);
+						valpop(&val);
+
+
+						char op = optop(oper);
+						oppop(&oper);
+						valpush(&val, calculate(val1, val2, op));
+					}
+					oppush(&oper, expr[i]);
+				}	
+		}
 		else if(expr[i] =='(') oppush(&oper, expr[i]);
 		else if(expr[i] == ')')
 		{
@@ -61,11 +69,10 @@ int solve(char* expr)
 
 				valpush(&val, calculate(val1, val2, op));
 
-			}
-			  oppop(&oper);
+					}
+					  oppop(&oper);
+				}
 		}
-
-	}
 
 	while(!opisEmpty(oper))
 	{
@@ -116,4 +123,43 @@ int exprLen(char *expr)
 		i++;
 	}
 	return len;
+}
+
+void sepUniandbin(char c, char d)
+{
+	oppush(&oper,c);
+	if(d == '-') uni = d;
+	return;
+}
+void pushDigit(char* expr, int len, int i)
+{
+	int value = 0;
+	while(i < len && isdigit(expr[i]))
+	{
+		value = value * 10 + (expr[i] - '0');
+		i++;
+	}
+	switch(uni)
+	{
+		case '-': value = value * (-1);
+					uni == ' ';
+					break;
+		case '+': value = value;
+					uni == ' ';
+					break;
+		case '*': value = value * (-1);
+					uni == ' ';
+					break;
+		case '/': value = value * (-1);
+					uni == ' ';
+					break;
+		case '%': value = value * (-1);
+					uni == ' ';
+					break;
+		case '^': value = value * (-1);
+					uni == ' ';
+		
+		//default: cout<<"Error in expression"<<endl;			
+	}
+	valpush(&val, value);
 }
--- exprSolver.h
+++ exprSolver.h
@@ -6,3 +6,5 @@ int solve(char *);
 int calculate(int, int, char);
 int preceed(char);
 int exprLen(char*);
+void sepUniandbin(char , char );
+void pushDigit(char*, int, int);
